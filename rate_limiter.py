d905b166426d4e864673f983bbd58f00f9b4e761
Nikolay Oshinov <noshinov@lwp.local>
2018-11-06 14:38:02 -0500

    Created wrapper to limit rate of API calls

---
+++ rate_limiter.py
@@ -0,0 +1,16 @@
+import time
+def limit_rate(fn):
+	"""
+	Limits number of times a function can be called per minute.
+	This is required for functions which make a call to Open Dota API
+	(Premium Tier access keys have 1200/minute calls).
+	Note this is wrapper is insufficient if two modules are
+	simultaneously making calls to API.
+	"""
+	def wrapper(*args, **kwargs):
+		args[0].api_calls += 1
+		if args[0] >= 1200:
+			time_since_first_call = min(time.time()-args[0].first_call, 60)
+			sleep(round(60-time_since_first_call) + 1)
+			args[0].first_call = time.time()
+		fn(*args, **kwargs)
