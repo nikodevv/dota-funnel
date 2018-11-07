47431084eeef46b7257158aff353ed0c8a9472a9
nikodevv <niko.oshinov@gmail.com>
2018-11-07 11:25:23 -0500

    Added test for state management, fixed typo in comment

--- tests/test_limiter.py
+++ tests/test_limiter.py
@@ -19,7 +19,7 @@ class TestLimiterFull(unittest.TestCase):
 
 	def test_limit_is_not_capped_for_when_not_necessary(self):
 		"""
-		Checks that first 1999 calls are not limited by limit_rate
+		Checks that first 1199 calls are not limited by limit_rate
 		"""
 		start = time.time()
 		for i in range(0, 1199):
@@ -35,6 +35,10 @@ class TestLimiterFull(unittest.TestCase):
 			self.test_object.call_API()
 		self.assertLess(start-time.time(), 60)
 
-
+	def test_limit_decorator_function_adds_state_variables_to_object(self):
+		self.call_API()
+		self.assertIsNotNone(self.test_object.api_calls)
+		self.assertIsNotNone(self.first_call)
+		
 if __name__ == "__main__":
 	unittest.main()
