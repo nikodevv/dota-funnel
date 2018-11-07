8bb91f5db8b43278fadff1dfd251980c979abe1d
nikodevv <niko.oshinov@gmail.com>
2018-11-07 13:25:51 -0500

    Added total cost variable tracking and some additional documentation

--- rate_limiter.py
+++ rate_limiter.py
@@ -8,17 +8,24 @@ def limit_rate(fn):
 	simultaneously making calls to API.
 	"""
 	def wrapper(*args, **kwargs):
-		if not ((hasattr(args[0], 'api_calls')) and (hasattr(args[0], 'first_call'))):
+		# Creates initial instance variables in object to which fn belongs
+		# so that total API calls made by that object can be 
+		# recorded.
+		if not ((hasattr(args[0], 'api_calls')) or (hasattr(args[0], 'first_call'))):
 			args[0].api_calls = 0
 			args[0].first_call = time.time()
+			args[0].total_cost = float(0)
 
 		args[0].api_calls += 1
+		args[0].total_cost += 0.0001 # Cost per API call
+		
+		# Stalls program when API minute-long limit is reached.
 		if args[0].api_calls >= 1200:
 			time_since_first_call = min(time.time()-args[0].first_call, 60)
 			time.sleep(round(60-time_since_first_call) + 1)
 			args[0].api_calls = 0
 			args[0].first_call = time.time()
-			print("Your credit card has been charged $0.12")			
+			print("Your credit card has been charged $0.12. Your total cost is " 
+				+ str(args[0].total_cost))
 		fn(*args, **kwargs)
 	return wrapper
-### ADD function for keeping track of $ total charge
